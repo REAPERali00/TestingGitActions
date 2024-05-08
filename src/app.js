6c70c8f0c70a34139a92f36faf88e4a588b82e22
REAPERali00 <alireza2000jadidi@gmail.com>
2024-05-08 12:14:29 -0400

    fixing the wrong input

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 
 app.get("/", (req, res) => {
-  return res.status(200).send("Hello Pie!");
+  return res.status(200).send("Hello world");
 });
 
 module.exports = app;
